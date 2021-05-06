1805193a768d0e9c834b663627921d61cf92ba9f
storrellas <storrellas@gmail.com>
2021-05-06 08:50:26 +0200

    ENH: Firt commit

---
+++ app/management/commands/export_pdf.py
@@ -0,0 +1,27 @@
+import io
+from PIL import Image
+# Django Imports
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+
+from ipyleaflet import Map, basemaps, basemap_to_tiles
+
+
+
+class Command(BaseCommand):
+    help = 'Generates a PDF file based on template'
+
+
+    def handle(self, *args, **options):
+        print("-- Generating tokens --")
+        m = Map(
+              basemap=basemap_to_tiles(basemaps.NASAGIBS.ModisTerraTrueColorCR, "2017-04-08"),
+              center=(52.204793, 360.121558),
+              zoom=4
+        )
+        m.save('my_map.html', title='My Map')
+
+        img_data = m._to_png(5)
+        img = Image.open(io.BytesIO(img_data))
+        img.save('image.png')
+
