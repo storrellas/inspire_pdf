eb37ded24c002624b36eccd60e8c26dc3d755e5a
storrellas <storrellas@gmail.com>
2021-05-06 09:56:46 +0200

    ENH: Sketching PDF

--- app/management/commands/export_pdf.py
+++ app/management/commands/export_pdf.py
@@ -3,9 +3,10 @@ from PIL import Image
 # Django Imports
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
+from django.template.loader import render_to_string
 
-from ipyleaflet import Map, basemaps, basemap_to_tiles
-
+# from ipyleaflet import Map, basemaps, basemap_to_tiles
+import pdfkit
 
 
 class Command(BaseCommand):
@@ -14,14 +15,16 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         print("-- Generating tokens --")
-        m = Map(
-              basemap=basemap_to_tiles(basemaps.NASAGIBS.ModisTerraTrueColorCR, "2017-04-08"),
-              center=(52.204793, 360.121558),
-              zoom=4
-        )
-        m.save('my_map.html', title='My Map')
+        # m = Map(
+        #       basemap=basemap_to_tiles(basemaps.NASAGIBS.ModisTerraTrueColorCR, "2017-04-08"),
+        #       center=(52.204793, 360.121558),
+        #       zoom=4
+        # )
+        # m.save('my_map.html', title='My Map')
 
-        img_data = m._to_png(5)
-        img = Image.open(io.BytesIO(img_data))
-        img.save('image.png')
+        # img_data = m._to_png(5)
+        # img = Image.open(io.BytesIO(img_data))
+        # img.save('image.png')
+        html_str = render_to_string('index.html')
+        pdfkit.from_string(html_str, 'out.pdf')
 
--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('about/', TemplateView.as_view(template_name="index.html")),
+    path('', TemplateView.as_view(template_name="index.html")),
 ]
