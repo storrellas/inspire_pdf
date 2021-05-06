8bc9f22d3834a0622f5bc45c4597896fc3ee7baf
storrellas <storrellas@gmail.com>
2021-05-06 10:43:00 +0200

    ENH: Adding requirements

--- app/management/commands/export_pdf.py
+++ app/management/commands/export_pdf.py
@@ -1,5 +1,4 @@
 import io
-from PIL import Image
 # Django Imports
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
@@ -14,7 +13,7 @@ class Command(BaseCommand):
 
 
     def handle(self, *args, **options):
-        print("-- Generating tokens --")
+        print("Generating PDF file:")
         # m = Map(
         #       basemap=basemap_to_tiles(basemaps.NASAGIBS.ModisTerraTrueColorCR, "2017-04-08"),
         #       center=(52.204793, 360.121558),
@@ -25,6 +24,15 @@ class Command(BaseCommand):
         # img_data = m._to_png(5)
         # img = Image.open(io.BytesIO(img_data))
         # img.save('image.png')
+
+        options = {
+            'encoding': 'UTF-8',        
+            'margin-left': '20mm',
+            'margin-right': '20mm',
+            'margin-bottom': '20mm',
+            'margin-top': '20mm'
+        }
+
         html_str = render_to_string('index.html')
-        pdfkit.from_string(html_str, 'out.pdf')
+        pdfkit.from_string(html_str, 'out.pdf', options=options)
 
